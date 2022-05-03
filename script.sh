706a758e5e593c3703a81244a3df240f5baf9445
Mykyta Martsevyi <53223543+nikmar13@users.noreply.github.com>
2022-05-03 19:12:09 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-date                                                                                                                                                                   
-ml icc                                                                                                                                                                 
-echo "Compiler option O0:"
+ml icc
+ls
+echo "Optimization 0"
 icc -O0 power.cpp -o power
-echo "Result:"
 time ./power
-rm power
-                                                                                                                                                                       
-echo ""
-echo "Compiler option O1:"
+echo -e "\nOptimization 1"
 icc -O1 power.cpp -o power
-echo "Result:"
 time ./power
-rm power
-                                                                                                                                                                       
-echo ""
-echo "Compiler option O2 :" 
+echo -e "\nOptimization 2"
 icc -O2 power.cpp -o power
-echo "Result:"
 time ./power
-rm power
-                                                                                                                                                                       
-echo ""
-echo "Compiler option O3 :" 
-icc -O3 power.cpp -ipo -o power
-echo "Result:"
+echo -e "\nOptimization 3"
+icc -O3 power.cpp -o power
 time ./power
-rm power
+list=$(lscpu | grep "Flags" | cut -d: -f2)
+IFS=' '
+read -ra flags <<< "$list"
+for flag in "${flags[@]}";
+do
+       f=${flag^^}
+       f=$(echo $f | tr "_" .)
+       icc power.cpp -x$f -o bin/out.$f
+done
+compiledFiles=$(ls bin)
+readarray -t files <<< "$compiledFiles"
+for file in "${files[@]}";
+do
+       echo "time ./bin/$file"
+       time "./bin/$file"
+done
